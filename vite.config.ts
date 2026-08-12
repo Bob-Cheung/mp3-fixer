1358026004fc586d4a11418af61174d42eb556eb
zl <zl@qq.com>
2026-08-12 15:41:32 +0800

    不适用GitHub访问，使用Cloudflare

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/mp3-fixer/',
+  // base: '/mp3-fixer/',
+  base: '/',
   plugins: [react()],
 })
